889ce26ce04def76eedb5163cbab36b68b472d12
DominiqueFlorence <ddaniell1987@gmail.com>
2021-11-18 14:49:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+
+var text = document.createTextNode("Dominique is the champion");
+newHeader.appendChild(text);
